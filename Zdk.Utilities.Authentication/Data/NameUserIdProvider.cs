6f6c3ecc857794be65044ac815f4e2b324393017
sknutsen <sondreknutsen1@gmail.com>
2022-01-17 18:11:20 +0100

    Authentication

---
+++ Zdk.Utilities.Authentication/Data/NameUserIdProvider.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Zdk.Utilities.Authentication.Data;
+
+public class NameUserIdProvider : IUserIdProvider
+{
+    public string? GetUserId(HubConnectionContext connection)
+    {
+        return connection.User?.Identity?.Name;
+    }
+}
---
+++ Zdk.Utilities.Authentication/ViewModels/Authorization/LogoutViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Zdk.Utilities.Authentication.ViewModels.Authorization;
+
+public class LogoutViewModel
+{
+    [BindNever]
+    public string RequestId { get; set; }
+}
