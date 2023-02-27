22b2cdbd6bb9b8779b904f68f27991bbae8c9b57
sknutsen <sondreknutsen1@gmail.com>
2023-02-27 20:01:17 +0100

    Introduced SignalR helper class lib

---
+++ Zdk.Utilities.SignalR/Base/BaseHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Zdk.Utilities.SignalR;
+
+public abstract partial class BaseHub<HubName> : Hub
+{
+    protected readonly ILogger<HubName> logger;
+
+    public BaseHub(IServiceProvider services)
+    {
+        logger = services.GetRequiredService<ILogger<HubName>>();
+    }
+}
---
+++ Zdk.Utilities.SignalR/Base/TransportGroups.cs
@@ -0,0 +1,28 @@
+﻿namespace Zdk.Utilities.SignalR;
+
+public abstract partial class BaseHub<HubName>
+{
+    public async Task JoinGroup()
+    {
+        int groupId = await GetGroupId();
+
+        await JoinGroup(groupId);
+    }
+
+    protected async Task JoinGroup(int groupId)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, groupId.ToString());
+    }
+
+    protected async Task LeaveGroup()
+    {
+        int groupId = await GetGroupId();
+
+        await LeaveGroup(groupId);
+    }
+
+    public async Task LeaveGroup(int groupId)
+    {
+        await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupId.ToString());
+    }
+}
